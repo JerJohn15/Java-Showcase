2ccdfe8208f5d7c9f0bdee3901db1429a691d18b
JerJohn15 <tjon2013@bu.edu>
2016-11-26 15:35:20 -0500

    updated fxml file, added controller

---
+++ Demo 2/SchedulerApp/src/Controller/UI/SchedulerUI.java
@@ -0,0 +1,33 @@
+/*
+ * @author - JerJohn15
+@details - This class contains the controller for SchedulerUI fxml.
+ */
+
+
+package Controller.UI;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+
+public class SchedulerUI {
+
+    @FXML
+    private JFXTextArea textArea;
+
+    @FXML
+    private JFXButton saveBtn;
+
+    @FXML
+    private JFXButton clearBtn;
+
+    @FXML
+    private JFXDatePicker dateField;
+
+    @FXML
+    private JFXTextField titleTxtField;
+
+}
+
