ffc10c123d893ff342c33484a2b76ebf3d5f317e
JerJohn15 <jthjohnson15@gmail.com>
2016-11-26 13:17:32 -0500

    added demo 2

---
+++ Demo 2/SchedulerApp/src/App/SchedulerApp.java
@@ -0,0 +1,30 @@
+/*
+ *A Schedulering Application.
+ */
+package App;
+
+import javafx.application.Application;
+
+import javafx.scene.Scene;
+
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author JerJohn15
+ */
+public class SchedulerApp extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
